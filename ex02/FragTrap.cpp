7e99eb9ac54392afe8bac585e30763f73c2d2c27
Jek John Tan <jetan@u91z04s04.42kl.edu.my>
2025-04-22 18:37:02 +0800

    implement highfiveguys function

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 18:01:00 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/22 18:15:32 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/22 18:36:28 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,37 @@ FragTrap::FragTrap(): ClapTrap()
 FragTrap::FragTrap(std::string name): ClapTrap(name)
 {
 	std::cout << "FragTrap Parameter constructor called" << std::endl;
-
+	HitPoints = 100;
+	EnergyPoints = 100;
+	AttackDamage = 30;
 }
 
 FragTrap::FragTrap(const FragTrap &other): ClapTrap(other)
 {
 	std::cout << "FragTrap Copy constructor called" << std::endl;
-
+	*this = other;
 }
 
 FragTrap &FragTrap::operator=(const FragTrap &other)
 {
 	std::cout << "FragTrap Copy assignment operator called" << std::endl;
-
+	
+	if (this != &other)
+	{
+		this->Name_ = other.Name_;
+		this->HitPoints = other.HitPoints;
+		this->EnergyPoints = other.EnergyPoints;
+		this->AttackDamage = other.AttackDamage;
+	}
+	return *this;
 }
 
 FragTrap::~FragTrap()
 {
 	std::cout << "FragTrap Destructor called" << std::endl;
+}
 
+void FragTrap::highFivesGuys(void)
+{
+	std::cout << Name_ << " request a positive high-fives" << std::endl;
 }
