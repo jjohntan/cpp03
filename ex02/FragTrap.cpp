9576514007c9c79422653b5987f476820be6ff24
Jek John Tan <jetan@u91z04s04.42kl.edu.my>
2025-04-22 18:16:51 +0800

    implement default, param, and copy constructor, copy assignment operator, and destructor

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,16 +6,37 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 18:01:00 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/22 18:02:36 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/22 18:15:32 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(/* args */)
+FragTrap::FragTrap(): ClapTrap()
 {
+	std::cout << "FragTrap Default constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(std::string name): ClapTrap(name)
+{
+	std::cout << "FragTrap Parameter constructor called" << std::endl;
+
+}
+
+FragTrap::FragTrap(const FragTrap &other): ClapTrap(other)
+{
+	std::cout << "FragTrap Copy constructor called" << std::endl;
+
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &other)
+{
+	std::cout << "FragTrap Copy assignment operator called" << std::endl;
+
 }
 
 FragTrap::~FragTrap()
 {
+	std::cout << "FragTrap Destructor called" << std::endl;
+
 }
