1d72bdfce8bdc5c72ac44fc944fa256d1bcc8e11
Jek John Tan <jetan@u91z04s02.42kl.edu.my>
2025-04-24 18:25:50 +0800

    add test cases and add getter function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 13:46:19 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/22 17:46:39 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/24 18:22:41 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 
 int main()
 {
-	// ClapTrap a;
-	// ClapTrap b( a );
-	// ClapTrap c;
+	// std::cout << "===== Orthodox Canonical Form ======" << std::endl;
+	// ScavTrap a("plant");
+	// ScavTrap b(a);
+	// ScavTrap c;
+	
 	// c = b;
 	
-	ScavTrap a("Foo");
-	ScavTrap b(a);
-	ScavTrap c;
-	c = b;
+	// std::cout << b.getName() << std::endl;
+	// std::cout << b.getHitPoints() << std::endl;
+	// std::cout << b.getEnergyPoints() << std::endl;
+	// std::cout << b.getAttackDamage() << std::endl;
+	std::cout << "========== ClapTrap ============" << std::endl;
+	ScavTrap a("plant");
+	a.attack("zombie");
+	a.takeDamage(3);
+	a.beRepaired(2);
 	a.guardGate();
 }
